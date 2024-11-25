d4b9b8e516d54eecc0a64d87c586dce65b4aa1e8
tonywzwz <161043062+tonywzwz@users.noreply.github.com>
2024-11-25 13:29:09 +0000

    Atividade Pronta

---
+++ project/models/Juridica.py
@@ -0,0 +1,16 @@
+import sys
+sys.path.append('/workspaces/Atividade-Pontuada-vdte/project/')
+from project.models.Endereco import Endereco
+from project.models.Pessoa import Pessoa
+from abc import ABC, abstractmethod
+
+class Juridica(Pessoa, ABC):
+    def __init__(self, id: int, nome: str, telefone: str, email: str, endereco: Endereco, cnpj:str, inscricaoEstadual:str) -> None:
+        super().__init__(id, nome, telefone, email, endereco)
+        self.cnpj = self._verificar_cnpj(cnpj)
+        self.inscricaoEstadual = inscricaoEstadual
+    
+    def _verificar_cnpj(self, cnpj):
+        if len(cnpj) > 14:
+              raise TypeError("Cnpj inválido.")
+        return cnpj
